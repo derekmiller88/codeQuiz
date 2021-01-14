570000e94c2746ede30f553dc533cb2c9383e1f9
Snow Day <derek.miller826@gmail.com>
2021-01-14 11:40:33 -0600

    updated readme.MD file with description of application and URL to live webpage on github

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # codeQuiz
+
+![codeQuiz screenshot](/assets/images/taskinatorScreenshot.jpg?raw=true "codeQuiz screenshot")
+
+## codeQuiz is a javascript-enabled quiz that was built using HTML, CSS, and Javascript. 
+
+This code quiz displays questions to the user and accepts input that is then used to determine a final score.
+
+There are three pages that are displayed during the course of the codeQuiz execution.
+
+The three pages are: Main Quiz, User Score Result, and Final Saved Score.
+
+The pages are dynamically generated using HTML DOM elements and Javascript functions.
+
+The quiz also features a timer that will count down from 60 to 0. This timer will automatically end the quiz when it reaches 0. It is also used to determine the user's score result. The user can also save their score result to a final score page and also clear their score if they want.
+
+### https://derekmiller88.github.io/codeQuiz/
+
+#### Developed by Derek Miller 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,29 @@
+/* global variable declarations here */
+/* timerEl used for the countdown function */
 var timerEl = document.getElementById('countdown');
-
 var timeLeft = 60;
+
+/* sets the feedback content when user selects correct/incorrect option */
 var feedbackHtmlCorrect = "<p>Correct!</p>";
 var feedbackHtmlIncorrect = "<p>Incorrect</p>";
 var element2 = document.getElementById("feedback");
 
+/* Sets the starting variables for the quiz. 
+these variables are used in determining whether or not we have completed the test and what question we are on */
 function Quiz(questions) {
     this.score = 0;
     this.questions = questions;
     this.questionIndex = 0;
 }
 
+/* pulls the current question the user is on */
 Quiz.prototype.getQuestionIndex = function() {
     return this.questions[this.questionIndex];
 }
 
+/* determines if the user selected the correct question
+also triggers the timer to reduce time when user selects incorrect answer
+also increments the score and questionIndex variables which are used to determine which question to display next */
 Quiz.prototype.guess = function(answer) {
     element2.style.display = 'block';
     if(this.getQuestionIndex().isCorrectAnswer(answer)) {
@@ -29,22 +38,28 @@ Quiz.prototype.guess = function(answer) {
     this.questionIndex++;
 }
 
+/* used to determine if we are at the end of the questions array
+if so, this will resolve to true and is used to stop the test and display the score */
 Quiz.prototype.isEnded = function() {
     return this.questionIndex === this.questions.length;
 }
 
+/* passes the user input to the variables used in the quiz */
 function Question(text, choices, answer) {
     this.text = text;
     this.choices = choices;
     this.answer = answer;
 }
 
+/* determines if the answer is correct */
 Question.prototype.isCorrectAnswer = function(choice) {
     return this.answer === choice;
 }
 
-
-
+/* main function of the quiz, continuously runs the for loop to move through the quiz questions
+if the quiz is not ended, will set the next quiz question and display
+determines if the quiz is ended by calling the isEnded function above and then runs the showScores function
+ */
 function populate() {
     if(quiz.isEnded()) {
         showScores();
@@ -64,6 +79,8 @@ function populate() {
     }
 };
 
+/* this function will assess what selection the user made 
+also run the populate function again to continuously loop through quiz questions*/
 function guess(id, guess) {
     var button = document.getElementById(id);
     button.onclick = function() {
@@ -72,6 +89,8 @@ function guess(id, guess) {
     }
 };
 
+/* dynamically displays the result of the user's test.
+displays the user score as well as input box for user ID and button to submit score*/
 function showScores() {
     stopFunction();
     var gameOverHTML = "<h1>All Done!</h1>";
@@ -81,6 +100,8 @@ function showScores() {
     element.innerHTML = gameOverHTML;
 };
 
+/* dynamically displays the final score page
+this page is used to display the user's saved high score */
 function finalScore() {
     var highScoreHTML = "<h1>HighScores</h1>";
     highScoreHTML += `<p><span id="finalScore">`+ document.querySelector('#textScore').value + ": " + timeLeft + `</span></p>`;
@@ -88,20 +109,13 @@ function finalScore() {
     var element = document.getElementById("quiz");
     
     element.innerHTML = highScoreHTML;
-    
-    //renderLastRegistered();
 }
 
-function renderLastRegistered() {
-    var finalScore = document.querySelector('#finalScore');
-    finalScore.textContent = initialInput;
-  }
-
-// Timer that counts down from 120
+// Timer function is used to count down from 60 seconds to 0
+//also used to end quiz and set score to 0 if user runs out of time
 var timeInterval = setInterval(countdown, 1000);
 function countdown() {
-  
-    // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+
       if(timeLeft>=1) {
         timerEl.textContent = "Time: " + timeLeft--;
       } else {
@@ -109,6 +123,8 @@ function countdown() {
       }
 };
 
+/* following two functions (sleep() and wait()) both used in the Quiz.prototype.guess function above
+the wait function is called to remove the feedback text after 1 second */
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -118,21 +134,24 @@ async function wait() {
     element2.style.display = "none";
 }
 
+/* clears out the list of saved user scores in the finalScore page */
 function clearScore() {
     var finalScore = document.querySelector('#finalScore');
     finalScore.textContent = "";
 }
 
+/* used in the goBack button on the finalScore page. used to literally go back to the score result page */
 function goBack() {
     populate();
 }
 
+/* used to stop the timer (which is also used as the user score) and remove the timer from being displayed */
 function stopFunction() {
     clearInterval(timeInterval);
     timerEl.textContent = "";
 }
 
-// create questions here
+// questions[] array used to generate questions and answers for quiz
 var questions = [
     new Question("What is the HTML tag under which one can write the JavaScript code?", ["1. javascript", "2. scripted","3. script", "4. js"], "3. script"),
     new Question("Choose the correct JavaScript syntax to change the content of the following HTML code.", ["1. document.getElement(“geek”).innerHTML=”I am a Geek”;", "2. document.getElementById(“geek”).innerHTML=”I am a Geek”;", "3. document.getId(“geek”)=”I am a Geek”;", "4. document.getElementById(“geek”).innerHTML=I am a Geek;"], "2. document.getElementById(“geek”).innerHTML=”I am a Geek”;"),
@@ -141,11 +160,11 @@ var questions = [
     new Question("Predict the output of the following JavaScript code.", ["1. 16", "2. Compilation Error", "3. 88", "4. Run Time Error"], "3. 88")
 ];
 
-// create quiz
+// creates quiz
 var quiz = new Quiz(questions);
 
-//start timer
+//starts timer
 countdown();
 
-// display quiz
+// displays and starts the quiz
 populate();
